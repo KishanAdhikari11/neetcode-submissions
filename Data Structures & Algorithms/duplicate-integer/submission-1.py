83a5c52d86268037eb55cc94a37b09edfbb0a3b0
Kishan Adhikari <61057393+KishanAdhikari11@users.noreply.github.com>
2026-09-22 16:51:23 +0545

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        counter={}
+        for i in nums:
+            if i in counter:
+                return True
+            else:
+                counter[i]=1
+        return False
+        
+            
+             
+            
+        
+
+         
